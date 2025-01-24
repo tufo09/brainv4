173ba2c775ebfd0caa7c1104ef90b5b9c57fa7b8
tufo09 <144875509+tufo09@users.noreply.github.com>
2025-01-24 23:27:49 +0100

    moved tests from main to brain_functions.h

--- src/brainV4.cpp
+++ src/brainV4.cpp
@@ -3,42 +3,13 @@
 #include <brainV4.h>
 #include <brain_functions.h>
 
-uint8_t distance_values[8];
-uint8_t count = 0;
-
- 
-void setup()
-{
+void setup() {
   KEPLERBRAIN_INIT();
   WRITE_LCD_CLEAR();
   WRITE_I2C_BNO055_INIT();
 }
  
-void loop()
-{
-  m_motor_write(0,100);
-  delay(1000);
-  m_motor_write(0,0);
-  m_motor_write(1,100);
-  delay(1000);
-  m_motor_write(1,0);
-  m_motor_write(2,100);
-  delay(1000);
-  m_motor_write(2,0);
-  m_motor_write(3,100);
-  delay(1000);
-  m_motor_write(3,0);
-
-
-  read_tof_sensors(distance_values);
-  WRITE_LCD_TEXT(1, 1, String(distance_values[5])+" "+String(distance_values[4])+" "+String(distance_values[3])+" "+String(distance_values[2]));
-  WRITE_LCD_TEXT(1, 2, String(distance_values[6])+" "+String(distance_values[7])+" "+String(distance_values[0])+" "+String(distance_values[1]));
-  if (count>100) {
-    count = 0;
-    WRITE_LCD_CLEAR();
-  }
-  // val6   val5    val4   val3
-  // val7   val8    val1    val2
-  count ++;
-
+void loop() {
+  TestCases testCases;
+  testCases.tof_display_raw();
 }
--- src/brain_functions.h
+++ src/brain_functions.h
@@ -45,3 +45,37 @@ void read_tof_sensors(uint8_t *distance_values) {
   }
   digitalWrite(SPI1, HIGH);
 }
+// test cases
+
+// tof_display_raw
+uint8_t distance_values[8];
+uint8_t count = 0;
+
+class TestCases {
+    public:
+        void tof_display_raw() {
+            read_tof_sensors(distance_values);
+            WRITE_LCD_TEXT(1, 1, String(distance_values[5])+" "+String(distance_values[4])+" "+String(distance_values[3])+" "+String(distance_values[2]));
+            WRITE_LCD_TEXT(1, 2, String(distance_values[6])+" "+String(distance_values[7])+" "+String(distance_values[0])+" "+String(distance_values[1]));
+            if (count>100) {
+                count = 0;
+                WRITE_LCD_CLEAR();
+            }
+            count ++;
+        }
+
+        void motor_switch() {
+                m_motor_write(0,100);
+                delay(1000);
+                m_motor_write(0,0);
+                m_motor_write(1,100);
+                delay(1000);
+                m_motor_write(1,0);
+                m_motor_write(2,100);
+                delay(1000);
+                m_motor_write(2,0);
+                m_motor_write(3,100);
+                delay(1000);
+                m_motor_write(3,0);
+        }
+};
