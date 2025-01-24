8810cf33eab5afa16e64e7cd85a4dd63562a2b4c
tufo09 <144875509+tufo09@users.noreply.github.com>
2025-01-24 22:58:13 +0100

    added tof redout

--- src/brain_functions.h
+++ src/brain_functions.h
@@ -3,7 +3,7 @@
 #include <KeplerBRAIN_V4.h>
 
 
-// multipliers, so the motors turn into the right direction
+// function to turn the motors in the right direction regardless of which direction the wires are connected
 int motor_multiplier_1 = -1;
 int motor_multiplier_2 = -1;
 int motor_multiplier_3 = 1;
@@ -25,4 +25,23 @@ void m_motor_write(int port, double value) {
         WRITE_MOTOR(M4, value*motor_multiplier_4);
         break;
     }
+}
+
+
+// function to pull the distance data from the expansion board connected via spi
+void read_tof_sensors(uint8_t *distance_values) {
+
+  // read 8 Bytes from TOF Sensorboard BEGIN
+  digitalWrite(SPI1, LOW);
+  if(spi.transfer(0XFF) == 250) { 
+    distance_values[0] = spi.transfer(0XFF);
+    distance_values[1] = spi.transfer(0XFF);
+    distance_values[2] = spi.transfer(0XFF);
+    distance_values[3] = spi.transfer(0XFF);
+    distance_values[4] = spi.transfer(0XFF);
+    distance_values[5] = spi.transfer(0XFF);
+    distance_values[6] = spi.transfer(0XFF);
+    distance_values[7] = spi.transfer(0XFF);
+  }
+  digitalWrite(SPI1, HIGH);
 }
