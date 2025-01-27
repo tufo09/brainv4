29c26833f130158c658d6119f8bd50bed31d516d
tufo09 <144875509+tufo09@users.noreply.github.com>
2025-01-27 21:42:44 +0100

    possibly fixing all the issues i caused with the merge

--- src/brainV4.cpp
+++ src/brainV4.cpp
@@ -4,14 +4,6 @@
 #include <brain_functions.h>
 
 TestCases testCases;
-Maze maze;
-Helper helper;
-
-int8_t offsets[8];
-
-
-TestCases testCases;
-Maze maze;
 Helper helper;
 
 int8_t offsets[8];
@@ -22,16 +14,8 @@ void setup()
   KEPLERBRAIN_INIT();
   WRITE_I2C_BNO055_INIT();
   WRITE_LCD_CLEAR();
-
-  helper.set_tof_offsets(offsets);
 }
 
 
 void loop() {
-  WRITE_LED(L2,1);
-  do { testCases.tof_display_raw();} while (READ_BUTTON_PRESSED(B2) != 1);
-  WRITE_LED(L2,0);
-  WRITE_LED(L3,1);
-  do { testCases.tof_display_processed(offsets);} while (READ_BUTTON_PRESSED(B2) != 1);
-  WRITE_LED(L3,0);
 }
