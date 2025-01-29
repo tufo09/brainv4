b3ed3bb3d661753ac9fa1548cddff54cda3bef41
tufo09 <144875509+tufo09@users.noreply.github.com>
2025-01-29 15:08:41 +0100

    can follow wall on the left side

--- src/brainV4.cpp
+++ src/brainV4.cpp
@@ -6,9 +6,6 @@
 TestCases testCases;
 Helper helper;
 
-int8_t offsets[8];
-uint8_t tof_read_arr[8];
-
 void setup()
 {
   KEPLERBRAIN_INIT();
@@ -18,6 +15,7 @@ void setup()
 
 void loop()
 {
+  drive_parallel();
 }
 
 // tof sensors on the roboter
@@ -28,40 +26,41 @@ void loop()
 
 // drive parallel to a wall
 // sides: 0 left; 1 right
-void drive_parallel(int side)
+void drive_parallel()
 {
-  read_tof_sensors(tof_read_arr);
-  // TODO: Modify the code for the sensor board to send in higher resolution. Right now it only sends in i think 1cm values, so one increment in a value is about a one centimetre increase in distance from the item it is measuring from. Code can be found at https://www.lbotics.at/keplerbrain-v4/sensoren/tof-abstandssensor.
-  switch (side)
+  uint8_t tof_read_arr[8];
+  int8_t ground_speed = 100;
+  int8_t speed_left, speed_right;
+  uint8_t target_distance = 15; // Target distance from the wall in centimeters
+
+  while (a_button_is_pressed() == 0)
   {
-  case 0:
-  int8_t ground_speed = 30;
-  int8_t speed_left_front;
-  int8_t speed_left_back;
-  int8_t last_speed_left_front;
-  int8_t last_speed_left_back;
-  uint8_t stop = 3;
+    read_tof_sensors(tof_read_arr);
 
-  // probably gonna only use the front sensor for now
-  uint8_t front_distance;
-  uint8_t back_distance;
+    uint8_t front_left_distance = tof_read_arr[5];
+    uint8_t back_left_distance = tof_read_arr[6];
 
-
-    // left side
-    // tof sensors to use are indexed with 5 and 6 in the array where five is the more forward sensor
-    do
+    // Adjust speed based on distance from the wall
+    if (front_left_distance > target_distance)
     {
-      read_tof_sensors(tof_read_arr);
-      front_distance = tof_read_arr[5];
-      back_distance = tof_read_arr[6];
+      speed_right = 100;
+      speed_left = 50;
+    }
+    else if (front_left_distance < target_distance)
+    {
+      speed_left = 100;
+      speed_right = 50;
+    }
+    else
+    {
+      speed_left = 70;
+      speed_right = 70;
+    }
 
-    } while (a_button_is_pressed() == 0);
-  
-    break;
-  int8_t speed_right_front;
-  int8_t speed_right_back;
-    // right side
-    // tof sensors to use are indexed with 2 and 1 in the array where two is the more foreward sensor
-    break;
+    // Write motor speeds
+    m_motor_write(0, speed_right); // Left motors
+    m_motor_write(1, speed_right); // Right motors
+    m_motor_write(2, speed_left);  // Left motors
+    m_motor_write(3, speed_left);  // Right motors
   }
 }
