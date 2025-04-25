b69091af538f41a5d9a14126600b3f69a209ffa0
tufo09 <144875509+tufo09@users.noreply.github.com>
2025-04-25 11:53:09 +0200

    read the imu

--- lib/UserLib/brain_functions.h
+++ lib/UserLib/brain_functions.h
@@ -62,6 +62,12 @@ void read_tof_sensors(uint8_t *distance_values)
     digitalWrite(SPI1, HIGH);
 }
 
+void read_imu(uint16_t *yaw, uint16_t *pitch, uint16_t *roll) {
+    yaw = READ_I2C_BNO055_YAW();
+    pitch = READ_I2C_BNO055_PITCH();
+    roll = READ_I2C_BNO055_ROLL();
+}
+
 // function to check if any button is pressed
 bool any_button_is_pressed()
 {
